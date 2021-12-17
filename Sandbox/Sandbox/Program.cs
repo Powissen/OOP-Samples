dfd5561be35c6c0c23536967288a4b6a40536fc2
Lukáš Lokša <lukasloksa2@gmail.com>
2021-12-17 08:57:18 +0100

    Add new objects

--- Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Program.cs
@@ -6,7 +6,10 @@ namespace Sample_01.Sandbox
     {
         static void Main(string[] args)
         {
-
+            Seahorse seahorse = new Seahorse();
+            Crab crab = new Crab();
+            Icecream icecream = new Icecream();
+            Shell shell = new Shell();
         }
     }
 }
